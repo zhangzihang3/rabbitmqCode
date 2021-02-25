907edf62e362b0f4cd50ecd5247bffc79a8731d4
zhangzixing <3273448110@qq.com>
2021-02-25 11:00:29 +0800

    Initial commit

---
+++ src/main/java/com/example/zzh/ZzhApplication.java
@@ -0,0 +1,13 @@
+package com.example.zzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zzh/ZzhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zzh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
